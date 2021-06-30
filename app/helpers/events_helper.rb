4cfb0e2527333057189538c4939728235f2b2b18
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-30 17:15:27 +0500

    Style User views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def create_event_btn
+    out = ''
+    if user_signed_in?
+      out << link_to('New Event', new_event_path, class: 'me-3 py-2 text-dark text-decoration-none')
+    end
+    out.html_safe
+  end
 end
