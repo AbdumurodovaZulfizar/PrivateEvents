e1b34d8dd5adb9b3d6bebee292c52798ab41f230
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-30 16:06:35 +0500

    Add other fields to tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_105434) do
+ActiveRecord::Schema.define(version: 2021_06_30_110543) do
 
   create_table "enrollments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
     t.index ["event_id"], name: "index_enrollments_on_event_id"
     t.index ["user_id"], name: "index_enrollments_on_user_id"
   end
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_06_30_105434) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_06_30_105434) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
