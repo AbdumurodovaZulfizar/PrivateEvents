afbf61f2be3b1a87c61e7b769cf6404ce1caa1af
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-02 19:51:09 +0500

    Correct association error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   def index
-    @created_events = current_user.events
-    @past_events = current_user.attended_events.previous
-    @upcoming_events = current_user.attended_events.upcoming
+    @users = User.all
   end
 end
