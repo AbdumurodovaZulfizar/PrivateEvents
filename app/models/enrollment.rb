205a257a2d96bd2f3dbd3b6ea70f308ff16cdd31
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-03 08:13:06 +0500

    Add enroll and cancel fuction

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,7 +1,5 @@
 class Enrollment < ApplicationRecord
   enum status: { invited: 0, accepted: 1 }
-  # belongs_to :attendee, class_name: "User"
-  # belongs_to :attended_event, class_name: "Event"
   belongs_to :user, optional: true
   belongs_to :event
 end
